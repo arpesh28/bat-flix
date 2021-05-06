71567270fc2ce7b4c1761dd9269b5fcffce21ea9
arpesh28 <arpeshgadekar@gmail.com>
2021-05-06 21:52:31 +0530

    Homepage Complete

--- src/screens/Home/HomePage.js
+++ src/screens/Home/HomePage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+import {Form, Button} from 'react-bootstrap'
 // External Modules
 import Carousel from 'react-elastic-carousel';
 
@@ -34,12 +34,12 @@ function HomePage() {
 
     console.log(recentInfo)
     const breakPoints = [
-        { width: 1, itemsToShow: 1 },
-        { width: 550, itemsToShow: 2, itemsToScroll: 2 },
-        { width: 768, itemsToShow: 3 },
-        { width: 1000, itemsToShow: 4 },
-        { width: 1400, itemsToShow: 5 },
-        { width: 1600, itemsToShow: 6 },
+        { width: 1, itemsToShow: 2 },
+        { width: 550, itemsToShow: 3},
+        { width: 768, itemsToShow: 5 },
+        { width: 1000, itemsToShow: 6 },
+        { width: 1400, itemsToShow: 8 },
+        { width: 1600, itemsToShow: 9 },
       ];
     return(
         <div className="homeContainer backgroundColor">
@@ -49,6 +49,12 @@ function HomePage() {
                <div className="bannerTextContainer">
                 <h2 className="white30 text-center">Discover Movies</h2>
                     <h4 className="sub20 text-center">BatFlix is an upcoming movie finder on demand service. A platform made with visually stunning design of our own. Stay Home & Stay Safe.</h4>
+                    <Form className="d-flex flex-row align-items-center mt-5">
+                        <Form.Control type="text" placeholder="Search for Movie" className="mr-sm-2 searchbox" />
+                        <Button variant="outline-info"
+                        className="searchbtn"
+                        >Search</Button>
+                    </Form>
             </div>
                </div>
             <div>
@@ -63,7 +69,7 @@ function HomePage() {
                                     item.poster_path&&
                                         <div
                                         key={item.id}
-                                        className="col-12 col-xs-6 col-sm-6 col-md-4 col-lg-3 col-xl-2 d-flex justify-content-center">
+                                        className="col-12 col-xs-6 col-sm-6 col-md-3 col-lg-1 col-xl-1 d-flex justify-content-center">
                                            <Movie  
                                                 type="latest"
                                                 image={item.poster_path} />
@@ -82,7 +88,7 @@ function HomePage() {
                                 item.backdrop_path&&(
                                     <div
                                     key={item.id}
-                                    className="col-12 col-xs-6 col-sm-6 col-md-4 col-lg-3 col-xl-3 d-flex justify-content-center">
+                                    className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 d-flex justify-content-center">
                                        <Movie  
                                             type="popular"
                                             image={item.backdrop_path}
